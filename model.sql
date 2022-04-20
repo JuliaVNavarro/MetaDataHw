1ca317fe4c29ed7095a3e62c27238e4b628590e3
Julia Navarro <79175907+JuliaVNavarro@users.noreply.github.com>
2022-04-19 21:45:59 -0700

    Update model.sql

--- model.sql
+++ model.sql
@@ -13,20 +13,20 @@ CREATE TABLE `relationschemes` (
     CONSTRAINT `rs_models_fk_01` FOREIGN KEY (`rsModelName`) REFERENCES `models` (`modelName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `types` (
-    `typeName` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`typeName`)
+CREATE TABLE `datatypes` (
+    `datatype` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`datatype`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `attributes` (
     `attributeName` VARCHAR(100) NOT NULL,
     `rsName` VARCHAR(100) NOT NULL,
     `modelName` VARCHAR(100) NOT NULL,
-    `typeName` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`attributeName`, `rsName`, `modelName`),
+    `datatype` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`attributeName`, `rsName`, `modelName`, `datatype`),
     CONSTRAINT `attributes_rs_fk_01` FOREIGN KEY (`rsName`) REFERENCES `relationschemes` (`rsName`),
     CONSTRAINT `attributes_rs_fk_02` FOREIGN KEY (`modelName`) REFERENCES `relationschemes` (`rsModelName`),
-    CONSTRAINT `attributes_types_fk_01` FOREIGN KEY (`typeName`) REFERENCES `types` (`typeName`)
+    CONSTRAINT `attributes_datatypes_fk_01` FOREIGN KEY (`datatype`) REFERENCES `datatypes` (`datatype`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `candidatekeys` (
@@ -50,3 +50,31 @@ CREATE TABLE `candidatekeyattributes` (
     CONSTRAINT `cka_attributes_fk_03` FOREIGN KEY (`attributeName`) REFERENCES `attributes` (`attributeName`),
     CONSTRAINT `cka_ck_fk_01` FOREIGN KEY (`ckName`) REFERENCES `candidatekeys` (`candidateKeyName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `decimals` (
+    `precision` INT NOT NULL,
+    `scale`     INT NOT NULL,
+    `modelName` VARCHAR(100) NOT NULL,
+    `rsName`    VARCHAR(100) NOT NULL,
+    `attributeName` VARCHAR(100) NOT NULL,
+    `datatype`  VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`modelName`, `rsName`, `attributeName`, `datatype`),
+    CONSTRAINT  `decimals_attributes_fk_01` FOREIGN KEY (`modelName`) REFERENCES `attributes` (`modelName`),
+    CONSTRAINT  `decimals_attributes_fk_02` FOREIGN KEY (`rsName`) REFERENCES `attributes` (`rsName`),
+    CONSTRAINT  `decimals_attributes_fk_03` FOREIGN KEY (`attributeName`) REFERENCES `attributes` (`rsName`),
+    CONSTRAINT  `decimals_attributes_fk_04`  FOREIGN KEY (`datatype`) REFERENCES `attributes` (`datatype`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `varchars` (
+    `length` INT NOT NULL,
+    `maxSize` INT NOT NULL,
+    `modelName` VARCHAR(100) NOT NULL,
+    `rsName` VARCHAR(100) NOT NULL,
+    `attributeName` VARCHAR(100) NOT NULL,
+    `datatype` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`modelName`, `rsName`, `attributeName`, `datatype`),
+    CONSTRAINT  `varchars_attributes_fk_01` FOREIGN KEY (`modelName`) REFERENCES `attributes` (`modelName`),
+    CONSTRAINT  `varchars_attributes_fk_02` FOREIGN KEY (`rsName`) REFERENCES `attributes` (`rsName`),
+    CONSTRAINT  `varchars_attributes_fk_03` FOREIGN KEY (`attributeName`) REFERENCES `attributes` (`rsName`),
+    CONSTRAINT  `varchars_attributes_fk_04`  FOREIGN KEY (`datatype`) REFERENCES `attributes` (`datatype`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
