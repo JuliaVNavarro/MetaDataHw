1b4f4c361c02781ff4befc62fc13e3c4c485fbee
dreysimp <92762249+dreysimp@users.noreply.github.com>
2022-05-10 23:57:13 -0700

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,7 @@
 -- the creation date defaults to NOW() if not specified
-CREATE TRIGGER `modelDate_BEFORE_INSERT` BEFORE INSERT ON `models` FOR EACH ROW BEGIN
-  IF(new.modelDate = NULL) THEN
-    SET new.modelDate = NOW();
+CREATE TRIGGER `model_date_BEFORE_INSERT` BEFORE INSERT ON `models` FOR EACH ROW BEGIN
+  IF(new.model_date IS NULL) THEN
+    SET new.model_date = NOW();
   END IF;
 END;
 
