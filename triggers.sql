50ae5918ef742b38b6999547c5a58906938b4dcf
Julia Navarro <79175907+JuliaVNavarro@users.noreply.github.com>
2022-05-10 23:50:30 -0700

    Update triggers.sql
    -- you cannot use a float attribute in a candidate key

--- triggers.sql
+++ triggers.sql
@@ -4,3 +4,12 @@ CREATE TRIGGER `modelDate_BEFORE_INSERT` BEFORE INSERT ON `models` FOR EACH ROW 
     SET new.modelDate = NOW();
   END IF;
 END;
+
+-- you cannot use a float attribute in a candidate key
+CREATE TRIGGER `dataType_BEFORE_INSERT` BEFORE INSERT ON `candidate_key_attributes` FOR EACH ROW BEGIN
+    IF(EXISTS (SELECT attribute_name
+        FROM attributes
+        WHERE attribute_name = new.attribute_name AND data_type = 'float')) THEN
+        SIGNAL SQLSTATE  '45000' SET MESSAGE_TEXT = 'You cannot have a float as a candidate key';
+    END IF;
+END;
