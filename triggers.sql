17cea037c0829cd49a58e75ea3445903ad484579
dreysimp <92762249+dreysimp@users.noreply.github.com>
2022-05-11 21:41:59 -0700

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -63,3 +63,77 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
+
+
+
+
+CREATE PROCEDURE `attributes_check_type`(
+	IN in_attribute_name VARCHAR(64), IN attribute_type VARCHAR(20))
+    /**
+    Validate that the attribute type value in the attribute matches the supplied value.
+    This is used in the on insert trigger for the varchar and decimal categories of
+    attribute.
+    @param		in_attribute_name	The attribute that we're checking.
+    @param		in_attribute_type	The type that it should have.
+    */
+BEGIN
+	DECLARE message VARCHAR(100);
+    IF (SELECT	count(*)
+		FROM	attributes
+        WHERE	attribute_name = in_attribute_name) <> 1 THEN
+		SIGNAL SQLSTATE '45000' set message_text = 'Error, unable to find that attribute';
+	ELSEIF (SELECT	data_type
+			FROM	attributes
+			WHERE	attribute_name = in_attribute_name) <> attribute_type THEN
+		SET message = CONCAT('Error, unable to set these properties for attribute that is not: ', attribute_type);
+		SIGNAL SQLSTATE '45000' set message_text = message;
+	END IF;
+END;
+
+CREATE TRIGGER `decimals_BEFORE_INSERT` BEFORE INSERT ON `decimals` FOR EACH ROW BEGIN
+	-- Make sure that this is a decimal category of a proper decimal attribute.
+	CALL attributes_check_type (new.attribute_name, 'decimal');
+END;
+
+CREATE TRIGGER `varchars_BEFORE_INSERT` BEFORE INSERT ON `varchars` FOR EACH ROW BEGIN
+	-- Make sure that this is a decimal category of a proper decimal attribute.
+	CALL attributes_check_type (new.attribute_name, 'varchar');
+END;
+
+CREATE PROCEDURE `attribute_complete`(IN v_attribute_name VARCHAR(64))
+/**
+	Check the supplied attribute to make sure that if it is a datatype that needs
+	additional information regarding the storage of the attribute, that we have
+	a corresponding row in the correct category table for that attribute.
+	@param	v_attribute_name	The namt of the attribute to check.
+*/
+BEGIN
+	IF NOT EXISTS (	SELECT	'X'
+					FROM	attributes
+                    WHERE	attribute_name = v_attribute_name) THEN
+		-- Attribute does not exist, no need to check further
+		SIGNAL SQLSTATE '45000' set message_text = 'Error, unable to find that attribute';
+	ELSEIF (	SELECT	data_type
+				FROM	attributes
+                WHERE	attribute_name = v_attribute_name) = 'decimal' AND
+			NOT EXISTS (	SELECT	'X'
+							FROM	decimals
+                            WHERE	attribute_name = v_attribute_name) THEN
+		-- It says it's a decimal, but the category entry is missing.
+		SIGNAL SQLSTATE '45000' set message_text = 'Error, missing precision and scale for this attribute!';
+	ELSEIF (	SELECT	data_type
+				FROM	attributes
+                WHERE	attribute_name = v_attribute_name) = 'varchar' AND
+			NOT EXISTS (	SELECT	'X'
+							FROM	varchars
+                            WHERE	attribute_name = v_attribute_name) THEN
+		-- It says it's a varchar, but the category entry is missing.
+		SIGNAL SQLSTATE '45000' set message_text = 'Error, missing length for this attribute!';
+	END IF;
+END;
+
+CREATE TRIGGER `attributes_BEFORE_UPDATE` BEFORE UPDATE ON `attributes` FOR EACH ROW BEGIN
+	IF new.data_type <> old.attribute_name THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error, you cannot change the type of an attribute!';
+	END IF;
+END;
