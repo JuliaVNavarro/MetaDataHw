e687eb5bcff3b22f0e06ea6c89b54c959061d446
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2022-05-11 20:31:52 -0700

    Create triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -18,20 +18,53 @@ END;
 
 CREATE TRIGGER `key_BEFORE_INSERT` BEFORE INSERT ON `candidate_key_attributes` FOR EACH ROW BEGIN
 
-  DECLARE ck_rs_name VARCHAR(30);
-  SELECT rs_name INTO ck_rs_name
-  FROM candidate_keys ck
-  WHERE ck.candidate_key_name = new.ck_name;
-  IF (new.rs_name != ck_rs_name) THEN
-    SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'A key can only have attributes that come from the same relation scheme as the key itself';
+    IF(EXISTS (
+        SELECT rs_name
+        FROM candidate_keys
+        WHERE candidate_key_name = new.candidate_key_name)) != new.rs_name)
+        THEN SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'a key can only have attributes that come from the same relation scheme as the key itself';
   END IF;
 END;
 
 /*
 --a key can only have attributes that come from the same relation scheme as the key itself
 
+
 4) A foreign key constraint cannot have the same name as a candidate key
 5) splitting the key
 */
+
+DELIMITER //
+CREATE TRIGGER `candidate_keys_BEFORE_INSERT` BEFORE INSERT ON `candidate_keys` FOR EACH ROW BEGIN
+    call get_key_name(new.candidate_key_name, new.model_name);
+END;
+
+CREATE TRIGGER `primary_keys_BEFORE_INSERT` BEFORE INSERT ON `primary_keys` FOR EACH ROW BEGIN
+    call get_key_name(new.pk_name, new.model_name);
+END;
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE `get_key_name` (IN in_constraint_name VARCHAR(50),
+                                            IN in_model_name VARCHAR(50))
+BEGIN
+    IF(SELECT COUNT(*) FROM candidate_keys
+        WHERE candidate_key_name= in_constraint_name
+        AND candidate_keys.candidate_key_name LIKE '%ck'
+        AND model_name = in_model_name) > 0
+    THEN
+        SIGNAL SQLSTATE '45000'
+        SET message_text = 'Error: There is already a candidate key with that constraint name.';
+    END IF;
+    IF (SELECT COUNT(*) FROM primary_keys
+        WHERE pk_name = in_constraint_name
+        AND model_name = in_model_name) > 0
+    THEN
+        SIGNAL SQLSTATE '45000'
+        SET message_text = 'Error: There is already a primary key with that constraint name.';
+    END IF;
+END //
+DELIMITER ;
 
